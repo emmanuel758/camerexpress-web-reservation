b7db0b91fd8743edd643710b9f4570a576e32f33
emmanuel758 <emmanuelleuna758@gmail.com>
2024-05-08 14:24:58 +0100

    done with navbar

--- src/app/Components/components/navbar/navbar.component.ts
+++ src/app/Components/components/navbar/navbar.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  toggle_menu() {
+    const body = document.getElementById("navbar");
+    if (body != null)
+      body.classList.toggle("navbar-mobile");
 
+    const elements = document.getElementsByClassName("mobile-nav-toggle");
+    elements[0].classList.toggle('d-none');
+  }
 }
