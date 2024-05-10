c909655bcd203e0ed373e7fdc91a0a850938a033
emmanuel758 <emmanuelleuna758@gmail.com>
2024-05-10 13:46:24 +0100

    done with home page frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Components/pages/home/home.component';
-import { FooterComponent } from './Components/components/footer/footer.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'footer', component: FooterComponent },
 
 ];
 
